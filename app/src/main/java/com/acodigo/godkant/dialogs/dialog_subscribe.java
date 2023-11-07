0f8bf713cc360f705503bc1f161bf3219d164ad2
ACodigo <inside.dragon.heart@gmail.com>
2023-11-07 16:29:45 +0100

    Initial commit

---
+++ app/src/main/java/com/acodigo/godkant/dialogs/dialog_subscribe.java
@@ -0,0 +1,16 @@
+package com.acodigo.godkant.dialogs;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.acodigo.godkant.R;
+
+public class dialog_subscribe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_subscribe);
+    }
+}
